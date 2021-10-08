2b4752b23d1f1cc6766d17e671897b0b7038a005
Drew Dabbs <dabbs.drew@gmail.com>
2021-10-08 01:50:08 -0400

    finished part 1_1

--- ProgramUI.cs
+++ ProgramUI.cs
@@ -8,8 +8,34 @@ namespace RickAndMortyGame
 {
     public class ProgramUI
     {
+        Dictionary<string, Room> Rooms = new Dictionary<string, Room>
+        {
+            {"garage", garage },
+            {"house", house },
+            {"driveway", driveway }
+        };
+        public static Room garage = new Room(
+            "\n\n\n\n\nYou're in the garage with all your junk and crap\n\n" +
+            "Obvious exits are DRIVEWAY and HOUSE\n",
+            new List<string> { "driveway", "house" }
+            );
+        public static Room driveway = new Room(
+            "\n\n\n\n\nYou're in the driveway. The car is gone but " +
+            "the oil stain is still there.\n\n" +
+            "Obvious exits are GARAGE and YARD\n",
+            new List<string> { "garage" }
+            );
+        public static Room house = new Room(
+            "\n\n\n\n\nYou're in the house now. It's drab and smells like \n" +
+            "lemon pine-sol with a hint of stale fart.\n\n" +
+            "Obvious exits are GARAGE and KITCHEN\n",
+            new List<string> { "garage" }
+            );
+
         public void Run()
         {
+            Room currentRoom = garage;
+
             Console.Clear();
             Console.WriteLine("You accidentally killed Morty. \n" +
                 "In order to construct a passable facimile, you must collect " +
@@ -18,10 +44,24 @@ namespace RickAndMortyGame
             bool alive = true;
             while (alive)
             {
-                string command = Console.ReadLine().ToLower();
                 Console.Clear();
+                Console.WriteLine(currentRoom.Splash);
+                string command = Console.ReadLine().ToLower();
+                
                 if (command.StartsWith("go ") || command.StartsWith("exit "))
                 {
+                    if (command.Contains("garage"))
+                    {
+                        currentRoom = garage;
+                    }
+                    else if (command.Contains("driveway"))
+                    {
+                        currentRoom = driveway;
+                    }
+                    else if (command.Contains("house"))
+                    {
+                        currentRoom = house;
+                    }
                     Console.WriteLine("Uh... Go Where?");
                 }
                 else if (command.StartsWith("get ") || command.StartsWith("take ") || command.StartsWith("grab"))
