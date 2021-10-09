34d0718ff4aac7940140fec321be1efb27dc7c8d
Drew Dabbs <dabbs.drew@gmail.com>
2021-10-09 00:34:29 -0400

    fixed and finished module 2.3

--- Mortys/WereMortys.cs
+++ Mortys/WereMortys.cs
@@ -29,7 +29,8 @@ namespace RickAndMortyGame.Mortys
                 Health -= attack.Damage;
             }
         }
-        public int Attack()
+        public Attack Attack()
+            // EFA -- This was... a challenge. It made a believer of me of naming with intent, if I wasn't before. Head is still a little off from this one but calling the class and the method 'Attack' I was not able to follow when this perfectly to find a fix for a long time. I understood it was the variable type 'int' as stated in the Module 2.3 that was causing the error, intellisense helped me that far. I just could not figure out a fix as the (Method)Attack returns two int's, a new enum, and a string. I just didn't understand where and how to start. Can I fix the (Class)Attack, or is it the (Method)Attack? Fun times, I am addicted to coffee again.
         {
             return new Attack(5, 10, DamageType.Slashing, "Paw Slap");
         }
@@ -41,13 +42,24 @@ namespace RickAndMortyGame.Mortys
         {
             Console.WriteLine("*gasp* My... my chest hurts...");
         }
-        public void Hurt(int damage)
+        public void Hurt(Attack attack)
         {
-            Health -= damage;
+            if (attack.Type == DamageType.Piercing || attack.Type == DamageType.Shocking)
+            {
+                Health -= (int)Math.Floor(attack.Damage * 2.5);
+            }
+            else if (attack.Type == DamageType.Bludgeoning || attack.Type == DamageType.Psych)
+            {
+                Health -= (int)Math.Ceiling(attack.Damage * 1.5);
+            }
+            else
+            {
+                Health -= attack.Damage;
+            }
         }
-        public int Attack()
+        public Attack Attack()
         {
-            return 2;
+            return new Attack(1, 2, DamageType.Bludgeoning, "Weak Punch");
         }
     }
 }
